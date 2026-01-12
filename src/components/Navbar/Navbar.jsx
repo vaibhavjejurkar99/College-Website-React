f18a087dd5162c0b79af09d55bf6b74922d8b085
vaibhavjejurkar100 <vaibhavjejurkar100@gmail.com>
2026-01-12 10:14:04 +0530

    Update in File

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
-        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Home</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
         <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
         <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
       </ul>
